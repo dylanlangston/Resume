6601fc9df104ba28aedf506995b2b514cd1d5eff
Dylan Langston <dylanlangston@gmail.com>
2025-09-14 02:36:57 +0000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM debian:stable-slim AS base
+USER root
+
+WORKDIR /root/
+
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+     && apt-get -y install --no-install-recommends ca-certificates bash curl unzip xz-utils git make zip chromium
+
+COPY ./src/package.json /root/src/package.json
+COPY ./src/bun.lock /root/src/bun.lock
+
+SHELL ["/bin/bash", "-lc"]
+
+RUN curl -L --proto '=https' --tlsv1.3 -sSf https://bun.sh/install | bash
+
+FROM base AS build
+COPY . /root/
+RUN make build
+
+FROM scratch AS publish
+COPY --from=build /root/dist /
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { promises as fs } from "fs";
+import {render as localTheme} from "jsonresume-theme-local";
+import puppeteer from "puppeteer";
+import { render } from "resumed";
+import { PDFDocument } from "pdf-lib";
+
+type Resume = Parameters<typeof render>[0];
+
+const filename = "../dist/resume.pdf";
+fs.mkdir("../dist").catch(() => {});
+
+const resume: Resume = JSON.parse(
+  await fs.readFile("resume.json", "utf-8")
+);
+
+const html: string = await render(resume, {
+  render: localTheme
+});
+
+const browser = await puppeteer.launch({
+  headless: true,
+  executablePath: "/usr/bin/chromium",
+  args: ["--headless", "--no-sandbox", "--disable-setuid-sandbox"]
+});
+const page = await browser.newPage();
+
+await page.setContent(html, { waitUntil: "networkidle0" });
+await page.pdf({ path: filename, format: "a4", printBackground: true });
+
+await browser.close();
+
+const raw = await fs.readFile(filename);
+const pdf = await PDFDocument.load(raw, { updateMetadata: true, });
+
+pdf.setTitle("Dylan Langston's Resume");
+pdf.setProducer("");
+pdf.setCreator("Dylan Langston");
+pdf.setAuthor("Dylan Langston");
+pdf.setSubject("Resume");
+pdf.setCreationDate(new Date("1998-03-22T00:00:00Z"));
+pdf.setKeywords(resume.skills?.flatMap(skill => skill.keywords).filter((name): name is string => typeof name === "string") ?? []);
+
+const optimized = await pdf.save({
+  useObjectStreams: true,
+  addDefaultPage: false,
+});
+await fs.writeFile(filename, optimized);
